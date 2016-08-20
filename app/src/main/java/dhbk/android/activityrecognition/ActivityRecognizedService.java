7819de029074a15670a121aa60b33cbe1ebac5da
Phong <p7157568@gmail.com>
2016-08-20 08:50:49 +0700

    feat- examine the activity and log it.

--- app/src/main/java/dhbk/android/activityrecognition/ActivityRecognizedService.java
+++ app/src/main/java/dhbk/android/activityrecognition/ActivityRecognizedService.java
@@ -2,13 +2,19 @@ package dhbk.android.activityrecognition;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.List;
+
 /**
  * Created by huynhducthanhphong on 8/19/16.
- *  todo 1 When Google Play Services returns the user's activity, it will be sent to this IntentService.
- *  This will allow you to perform your application logic in the background as the user goes about their day.
+ * todo 1 When Google Play Services returns the user's activity, it will be sent to this IntentService.
+ * This will allow you to perform your application logic in the background as the user goes about their day.
  */
 
 /**
@@ -40,52 +46,53 @@ public class ActivityRecognizedService extends IntentService {
      */
     @Override
     protected void onHandleIntent(Intent intent) {
-        if(ActivityRecognitionResult.hasResult(intent)) {
+        if (ActivityRecognitionResult.hasResult(intent)) {
+            // intent chứa loại hành dông (đi, chạy) sau môi 3s, thì service sẽ phân tích trong background và send notification
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
-            handleDetectedActivities( result.getProbableActivities() );
+            handleDetectedActivities(result.getProbableActivities());
         }
     }
 
     private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
-        for( DetectedActivity activity : probableActivities ) {
-            switch( activity.getType() ) {
+        for (DetectedActivity activity : probableActivities) {
+            switch (activity.getType()) {
                 case DetectedActivity.IN_VEHICLE: {
-                    Log.e( "ActivityRecogition", "In Vehicle: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "In Vehicle: " + activity.getConfidence());
                     break;
                 }
                 case DetectedActivity.ON_BICYCLE: {
-                    Log.e( "ActivityRecogition", "On Bicycle: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "On Bicycle: " + activity.getConfidence());
                     break;
                 }
                 case DetectedActivity.ON_FOOT: {
-                    Log.e( "ActivityRecogition", "On Foot: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "On Foot: " + activity.getConfidence());
                     break;
                 }
                 case DetectedActivity.RUNNING: {
-                    Log.e( "ActivityRecogition", "Running: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Running: " + activity.getConfidence());
                     break;
                 }
                 case DetectedActivity.STILL: {
-                    Log.e( "ActivityRecogition", "Still: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Still: " + activity.getConfidence());
                     break;
                 }
                 case DetectedActivity.TILTING: {
-                    Log.e( "ActivityRecogition", "Tilting: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Tilting: " + activity.getConfidence());
                     break;
                 }
                 case DetectedActivity.WALKING: {
-                    Log.e( "ActivityRecogition", "Walking: " + activity.getConfidence() );
-                    if( activity.getConfidence() >= 75 ) {
+                    Log.e("ActivityRecogition", "Walking: " + activity.getConfidence());
+                    if (activity.getConfidence() >= 75) {
                         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                        builder.setContentText( "Are you walking?" );
-                        builder.setSmallIcon( R.mipmap.ic_launcher );
-                        builder.setContentTitle( getString( R.string.app_name ) );
+                        builder.setContentText("Are you walking?");
+                        builder.setSmallIcon(R.mipmap.ic_launcher);
+                        builder.setContentTitle(getString(R.string.app_name));
                         NotificationManagerCompat.from(this).notify(0, builder.build());
                     }
                     break;
                 }
                 case DetectedActivity.UNKNOWN: {
-                    Log.e( "ActivityRecogition", "Unknown: " + activity.getConfidence() );
+                    Log.e("ActivityRecogition", "Unknown: " + activity.getConfidence());
                     break;
                 }
             }
